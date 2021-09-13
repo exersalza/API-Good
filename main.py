2b5c7175ca806b981b19790ce9cdfa9b94aabb21
exersalza <squidodle44@gmail.com>
2021-09-13 18:43:52 +0200

    Added Switch in EVERYTHING

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from urlscan.scangrep import urlscan
+from urlscan.url_grep import urlscan
+from switch import Switch
 
 
+def main(val=input('Input what you want: ').lower().strip(' ')):
 
-if input('Input what you want: ').lower() == 'url':
-    i = input('URL to Prove: ')
+    # val = input('Input what you want: ').lower().strip(' ')
 
-    if '400' in str(urlscan(i)):
-        print('coggers')
+    with Switch(val) as case:
+
+        if case('url'):
+            i = input('URL to Prove: ')
+
+            def url_switcher(url=urlscan(str(i))):
+                values = []
+
+                with Switch(str(url).strip('<Response > [ ]')) as url_case:
+                    if url_case('400'):
+                        values.append('dont exists or is not reachable.')
+                    if url_case('200'):
+                        values.append('passed')
+
+                return values
+
+            print(str(url_switcher()).strip('[\']'))
+
+
+main()
+
