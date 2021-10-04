3d97ec47014b24052af877fba974331a2b345e6f
exersalza <>
2021-10-04 09:01:11 +0000

    IT FCKING WORKS

--- startup.lua
+++ startup.lua
@@ -1,27 +1,37 @@
 path1 = "DC-Bot/cogs/etc/config.py"
 path2 = "DC-Bot/API/config.py"
+path3 = "DC-Bot/cogs/etc/.env"
 
-if not os.isfile(path1) then
 
-    config1_file = io.open(path1, "w")
-    config1_file:write("TOKEN = ''\nPREFIX = ''\nESCAPE = ''")
-    config1_file:close()
+local option = io.read()
 
-else
+if option == 'standart' then
+  print(option)
+  config1_file = io.open(path1, "w")
+  config1_file:write("TOKEN = ''\nPREFIX = ''\nESCAPE = ''")
+  config1_file:close()
 
-    print("File ", path1, " already exists")
+  config2_file = io.open(path2, "w")
+  config2_file:write("URLSCAN_TOKEN = ''\nIQAIR = ''")
+  config2_file:close()
 
+elseif option == 'repl' then
+  config1_file = io.open(path1, "w")
+  config1_file:write("import os\n\ndef token_grep(value):\n  return os.getenv(value)\n\nTOKEN = token_grep(value)\nPREFIX = ''\nESCAPE = ''\n")
+  config1_file:close()
+
+  config2_file = io.open(path2, "w")
+  config2_file:write("URLSCAN_TOKEN = ''\nIQAIR = ''")
+  config2_file:close()
+
+  config3_file = io.open(path3, "w")
+  config3_file:write("TOKEN=''")
+  config3_file:close()
 end
 
-if not os.isfile(path2) then
 
-    config2_file = io.open(path2, "w")
-    config2_file:write("URLSCAN_TOKEN = ''\nIQAIR = ''")
-    config2_file:close()
 
-else
-
-    print("File ", path2, " already exists")
-
-end
+--[[ config2_file = io.open(path2, "w")
+config2_file:write("URLSCAN_TOKEN = ''\nIQAIR = ''")
+config2_file:close() ]]
 
