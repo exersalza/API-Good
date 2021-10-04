aeaa2980f1efe9b560b8a528499584fea718e70f
exersalza <>
2021-10-04 12:27:00 +0000

    warum mache ich das hier?

--- startup.py
+++ startup.py
@@ -1,8 +1,36 @@
 import os
 
 ENV_FILE = 'DC-Bot/cogs/etc/.env'
-FILE1 = 'DC-Bot/cogs/etc/config.py'
-FILE2 = 'DC-Bot/API/config.py'
+FILE1 = 'DC-Bot/API/config.py'
+FILE2 = 'DC-Bot/cogs/etc/config.py'
+
+
+ENV_TEMPLATE = open('templates/templateENV', 'r')
+TEMPLATE1 = open('templates/templateAPICONFIG', 'r')
+TEMPLATE2 = open('templates/templateBOTCONFIG', 'r')
+TEMPLATE3 = open('templates/templateBOTCONFIGv2', 'r')
+
+
+def setup(path, template):
+  if os.path.isfile(path):
+    return f'File {path} exists!'
+
+  else:
+    f = open(path, 'w')
+    for i in template:
+      f.write(i)
+    
+    f.close()
 
 
 
+if __name__ == '__main__':
+  _input = input('Option: ')
+  if _input == 'normal':
+    setup(ENV_FILE, ENV_TEMPLATE)
+    setup(FILE1, TEMPLATE1)
+    setup(FILE2, TEMPLATE2)
+  elif _input == 'flask':
+    print('if you poure water on a rock, nothing happens!')  # if you poure water on a rock, nothing happens!
+  else:
+    raise ValueError('Wrong input. Possible inputs: normal, flask')
