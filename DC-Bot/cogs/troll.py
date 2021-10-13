121aae4912b677c2bd295640dc857e9dac0f6223
exersalza <squidodle44@gmail.com>
2021-10-13 20:56:57 +0200

    Troll

--- DC-Bot/cogs/troll.py
+++ DC-Bot/cogs/troll.py
@@ -1,13 +1,29 @@
 import nextcord
 from nextcord.ext import commands
-
+from nextcord.errors import HTTPException
 
 # todo:
 #   Voice troll option
 
+
 class Troll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(aliases=['MONKEY', 'Monkie'])
+    async def monkie(self, ctx):
+        channel = ctx.author.voice.channel
+        await channel.connect()
+
+    @commands.Command
+    async def join(self, ctx):
+        channel = ctx.author.voice.channel
+        await channel.connect()
+
+
+    @commands.Command
+    async def leave(self, ctx):
+        await ctx.voice_client.disconnect()
 
 
 def setup(bot):
